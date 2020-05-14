8e5c619b13ed53d32b29336b588e86efd0048303
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2020-05-14 16:03:37 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 #TODO: add the code for the ApScheduler here
 def scheduled_task():
-    return "It is now 4:03pm</p>"
+    return "It is now 4:06pm</p>"
  
 @app.route('/')
 def welcome():
    scheduler = BackgroundScheduler({'apscheduler.timezone':'America/Los_Angeles'})
    scheduler.start()
-   scheduler.add_job(scheduled_task, trigger='cron', hour=16, minute=3)
+   scheduler.add_job(scheduled_task, trigger='cron', hour=16, minute=6)
    return render_template('home.html')
   
 if __name__=="__main__":
