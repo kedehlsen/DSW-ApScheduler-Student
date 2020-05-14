e817b7f3eb0d80134445816111c6e6274a8eddda
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2020-05-14 15:10:39 -0700

    changed the Markup return on line 12 to a var

--- webapp.py
+++ webapp.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 
 #TODO: add the code for the ApScheduler here
 def scheduled_task():
-    return Markup("<p>It is now 3:02pm</p>")
+    timed_message = Markup("<p>It is now 3:02pm</p>")
  
 @app.route('/')
 def welcome():
    scheduler = BackgroundScheduler({'apscheduler.timezone':'America/Los_Angeles'})
    scheduler.start()
    scheduler.add_job(scheduled_task, trigger='cron', hour=15, minute=2)
-   return render_template('home.html')
+   return render_template('home.html', timed_message= message)
   
 if __name__=="__main__":
     app.run(debug=False)
